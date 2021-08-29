b81322e9b56ea1031bd41f48a172c194bd120458
laorelbetty@gmail.com <laorelbetty@gmail.com>
2021-08-29 16:43:09 -0700

    init

--- src/App.js
+++ src/App.js
@@ -5,18 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>SushiSol</h1>
+        <p>Coming soon...</p>
+        <form action="https://google.com">
+          <input type="submit" value="Twitter" />
+        </form>
+        <form action="https://google.com">
+          <input type="submit" value="Discord" />
+        </form>
       </header>
     </div>
   );
